5a9ce4e4f224dd8126f64caf748f7d2a8ec5cfd4
Karen Vargas <kalivargas.11@gmail.com>
2023-01-30 22:27:15 -0500

    Ejercicio Finalizado con validaciones

--- main.js
+++ main.js
@@ -1,27 +1,132 @@
 
 function calculoPerimetroCuadrado (){
+    let input = document.getElementById('side');  
+    let value = parseInt(input.value);
 
-    let input = document.getElementById('side');      //Se alamcena en variabl el id del input
-    let value = Number(input.value);                  // Se convierte en numero el valor del obtenido del input
-    
-    let calculoPerimetro = value * value;             //Operacion del perimetro
-                                                
-Swal.fire(`El resulatdo de Cuadrado es: ${calculoPerimetro}`)
-   const message = `El resulatdo de Cuadrado es: ${calculoPerimetro}`;    //Se crea el mensaje con la funcion especifica 
- document.getElementById('result').innerHTML = message;              //Se retorna mesaje al HTML con innerHtml
-
+    //Validacion
+     if (!Number.isInteger(value)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You can only enter numbers',
+      })
+    } else {
+      let calculo= value * value;                                                       
+      Swal.fire(`The result of the perimeter of the square is: ${calculo}`)
+    }
 }
 
-
 function calculoAreaCuadrado (){
+    let input = document.getElementById('side');  
+    let value = parseInt(input.value);
 
-    let input = document.getElementById('side');      //Se alamcena en variabl el id del input
-    let value = Number(input.value);                  // Se convierte en numero el valor del obtenido del input
-    
-    let calculoArea = value * 2;             //Operacion del perimetro
-                                                
-   Swal.fire(`El resulatdo de Cuadrado es: ${calculoArea}`)
-   const message = `El resulatdo de Cuadrado es: ${calculoArea}`;    //Se crea el mensaje con la funcion especifica 
- document.getElementById('result').innerHTML = message;              //Se retorna mesaje al HTML con innerHtml
+  //Validacion
+   if (!Number.isInteger(value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo = value * 2;                                                       
+    Swal.fire(`The result of the area of the square is: ${calculo}`)
+  }
+   
+}
 
+function calculoPerimetroTriangulo () {
+  let input1 = document.getElementById('side1');  
+  let input2 = document.getElementById('side2');  
+  let input3 = document.getElementById('side3');  
+
+  let value1 = parseInt(input1.value);
+  let value2 = parseInt(input2.value);
+  let value3 = parseInt(input3.value);
+
+
+  //Validacion
+   if (!Number.isInteger(value1)&!Number.isInteger(value2)||!Number.isInteger(value3)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo= value1 + value2 + value3;                                                       
+    Swal.fire(`The result of the perimeter of the triangle is: ${calculo}`)
+  }
+}
+
+function calculoAreaTriangulo(){
+  let input1 = document.getElementById('base');  
+  let input2 = document.getElementById('altura');  
+
+  let value1 = parseInt(input1.value);
+  let value2 = parseInt(input2.value);
+
+
+  //Validacion
+   if (!Number.isInteger(value1)||!Number.isInteger(value2)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo= (value1 * value2)/2;                                                       
+    Swal.fire(`The result of the area of the triangle is: ${calculo}`)
+  }
+}
+
+function calculoDiametroCirculo(){
+  let input = document.getElementById('lado');  
+  let value = parseInt(input.value);
+
+  //Validacion
+   if (!Number.isInteger(value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo= value * 2                                                       
+    Swal.fire(`The result of the area of the triangle is: ${calculo}`)
+  }
+}
+
+function calculoAreaCirculo(){
+  let input = document.getElementById('lado');  
+  let value = parseInt(input.value);
+
+
+  //Validacion
+   if (!Number.isInteger(value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo= (value * value) * Math.PI     
+    let result = calculo;                                                  
+    Swal.fire(`The result of the area of the triangle is: ${calculo}`)
+  }
+}
+
+function calculoPerimetroCirculo(){
+  let input = document.getElementById('lado');  
+  let value = parseInt(input.value);
+
+  //Validacion
+   if (!Number.isInteger(value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You can only enter numbers',
+    })
+  } else {
+    let calculo= (value * 2) * Math.PI;                                                       
+    Swal.fire(`The result of the area of the triangle is: ${calculo}`)
+  }
 }
